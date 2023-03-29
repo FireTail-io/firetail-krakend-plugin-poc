61408859c66af12b69d5a45750addb6ec5bfb859
theteacat <joshua@firetail.io>
2023-03-29 15:14:46 +0100

    Remove unused boilerplate

--- firetail-krakend-plugin/main.go
+++ firetail-krakend-plugin/main.go
@@ -64,36 +64,3 @@ func (r registerer) registerHandlers(_ context.Context, extra map[string]interfa
 	// Return the handler wrapped in the firetail middleware
 	return firetailMiddleware(h), nil
 }
-
-func main() {}
-
-// This logger is replaced by the RegisterLogger method to load the one from KrakenD
-var logger Logger = noopLogger{}
-
-func (registerer) RegisterLogger(v interface{}) {
-	l, ok := v.(Logger)
-	if !ok {
-		return
-	}
-	logger = l
-	logger.Debug(fmt.Sprintf("[PLUGIN: %s] Logger loaded", HandlerRegisterer))
-}
-
-type Logger interface {
-	Debug(v ...interface{})
-	Info(v ...interface{})
-	Warning(v ...interface{})
-	Error(v ...interface{})
-	Critical(v ...interface{})
-	Fatal(v ...interface{})
-}
-
-// Empty logger implementation
-type noopLogger struct{}
-
-func (n noopLogger) Debug(_ ...interface{})    {}
-func (n noopLogger) Info(_ ...interface{})     {}
-func (n noopLogger) Warning(_ ...interface{})  {}
-func (n noopLogger) Error(_ ...interface{})    {}
-func (n noopLogger) Critical(_ ...interface{}) {}
-func (n noopLogger) Fatal(_ ...interface{})    {}
